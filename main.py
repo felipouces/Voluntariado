cf90e61b8c484b564910aabc5442177ebf9542f8
felipouces <felipebody-boardhotmail.com>
2025-05-28 17:07:21 +0100

    Implementa funcionalidade de teste de login

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from views.login_view import iniciar_login_view
 if __name__ == "__main__":
     iniciar_login_view()
 
-# Este é um comentário temporário para forçar um novo commit
+# Este é um comentário temporário para forçar um novo commit2
